ab68d927157a5d7e12f9c6c0ac58122d8863e1bc
shirku <robin.barbulla@gmail.com>
2023-09-26 18:35:45 +0200

    REFRESH_TOKEN_LIFETIME to 1 week

--- settings/settings.py
+++ settings/settings.py
@@ -156,7 +156,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=1),
     "SIGNING_KEY": SECRET_KEY,
     "USER_ID_FIELD": "address",
     "ROTATE_REFRESH_TOKENS": True,
